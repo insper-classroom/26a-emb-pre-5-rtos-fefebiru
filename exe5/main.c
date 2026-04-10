3b7c9114b349a91b6235adf1a855fb1adc1a4158
fefebiru <felipet4@al.insper.edu.br>
2026-04-10 10:14:55 -0300

    exe5

--- exe5/main.c
+++ exe5/main.c
@@ -23,24 +23,103 @@ const int BTN_PIN_Y = 21;
 const int LED_PIN_R = 5;
 const int LED_PIN_Y = 10;
 
+QueueHandle_t xQueueButId;
+SemaphoreHandle_t xSemaphore_r;
+
+SemaphoreHandle_t xSemaphore_y;
+
+void btn_callback(uint gpio, uint32_t events) {
+    if (events == 0x4) { // fall edge
+        if (gpio == BTN_PIN_R) {
+            xSemaphoreGiveFromISR(xSemaphore_r, 0);
+        } 
+        else if (gpio == BTN_PIN_Y) {
+            xSemaphoreGiveFromISR(xSemaphore_y, 0);
+        }
+    }
+}
+
+void led_r_task(void* p) {
+    gpio_init(LED_PIN_R);
+    gpio_set_dir(LED_PIN_R, GPIO_OUT);
+
+    int piscando = 0;
+    while (true) {
+        if (xSemaphoreTake(xSemaphore_r, 0) == pdTRUE) {
+            piscando = !piscando;
+        }
+
+        if (piscando) {
+            gpio_put(LED_PIN_R, 1);
+            vTaskDelay(pdMS_TO_TICKS(100));
+            gpio_put(LED_PIN_R, 0);
+            vTaskDelay(pdMS_TO_TICKS(100));
+        } else {
+            gpio_put(LED_PIN_R, 0);
+        }
+    }
+}
+
+void led_y_task(void* p) {
+    gpio_init(LED_PIN_Y);
+    gpio_set_dir(LED_PIN_Y, GPIO_OUT);
+
+    int piscando = 0;
+    while (true) {
+        if (xSemaphoreTake(xSemaphore_y, 0) == pdTRUE) {
+            piscando = !piscando;
+        }
+
+        if (piscando) {
+            gpio_put(LED_PIN_Y, 1);
+            vTaskDelay(pdMS_TO_TICKS(100));
+            gpio_put(LED_PIN_Y, 0);
+            vTaskDelay(pdMS_TO_TICKS(100));
+        } else {
+            gpio_put(LED_PIN_Y, 0);
+        }
+    }
+}
 
 void btn_task(void* p) {
+    gpio_init(BTN_PIN_R);
+    gpio_set_dir(BTN_PIN_R, GPIO_IN);
+    gpio_pull_up(BTN_PIN_R);
+    gpio_set_irq_enabled_with_callback(BTN_PIN_R, GPIO_IRQ_EDGE_FALL, true, &btn_callback);
 
+    gpio_init(BTN_PIN_Y);
+    gpio_set_dir(BTN_PIN_Y, GPIO_IN);
+    gpio_pull_up(BTN_PIN_Y);
+    gpio_set_irq_enabled_with_callback(BTN_PIN_Y, GPIO_IRQ_EDGE_FALL, true, &btn_callback);
+
+    int id;
     while (true) {
-
+        if (xQueueReceive(xQueueButId, &id, portMAX_DELAY) == pdTRUE) {
+            if (id == BTN_PIN_R)
+                xSemaphoreGive(xSemaphore_r);
+            else if (id == BTN_PIN_Y)
+                xSemaphoreGive(xSemaphore_y);
+        }
     }
 }
 
 
-
 int main() {
     stdio_init_all();
+    printf("Start RTOS \n");
 
-    xTaskCreate(btn_task, "BTN_Task 1", 256, NULL, 1, NULL);
+    xQueueButId = xQueueCreate(32, sizeof(int));
+    xSemaphore_r = xSemaphoreCreateBinary();
+
+    xSemaphore_y = xSemaphoreCreateBinary();
+
+    xTaskCreate(btn_task, "BTN_Task", 256, NULL, 1, NULL);
+    xTaskCreate(led_r_task, "LED_Task R", 256, NULL, 1, NULL);
+    xTaskCreate(led_y_task, "LED_Task Y", 256, NULL, 1, NULL);
+
 
     vTaskStartScheduler();
 
-    while(1){}
-
-    return 0;
+    while (true)
+        ;
 }
